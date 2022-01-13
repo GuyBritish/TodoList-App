441c970df86ca8acdc1638d8a5ca3937d28f18e5
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-13 00:00:17 -0600

    Add todo context

---
+++ src/store/todoContext.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import Todo from "../models/todo";
+
+type todoContextDef = {
+	items: Todo[];
+	addTodo: (text: string) => void;
+	removeTodo: (id: string) => void;
+};
+
+const TodoContext = React.createContext<todoContextDef>({
+	items: [],
+	addTodo: () => {},
+	removeTodo: (id: string) => {},
+});
+
+const TodoContextProvider: React.FC = (props) => {
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const addTodoHandler = (text: string) => {
+		const newTodo = new Todo(text);
+		setTodos((prev) => {
+			return prev.concat(newTodo);
+		});
+	};
+
+	const removeTodoHandler = (id: string) => {
+		setTodos((prev) => {
+			return prev.filter((todo) => {
+				return todo.id !== id;
+			});
+		});
+	};
+
+	const ctxValue: todoContextDef = {
+		items: todos,
+		addTodo: addTodoHandler,
+		removeTodo: removeTodoHandler,
+	};
+
+	return <TodoContext.Provider value={ctxValue}>{props.children}</TodoContext.Provider>;
+};
+
+export default TodoContextProvider;
